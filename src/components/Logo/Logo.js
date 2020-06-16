af4683b1b6ccb2e57ebb6956a0d14f3a7cf9f194
indre101 <zygaityte.indre@gmail.com>
2020-06-16 17:31:59 +0200

    plays the logo in intervals

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,31 +1,32 @@
 // Logo Animation Lottie
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import animationData from "../../assets/lottifiles/data08.json";
 import lottie from "lottie-web";
 export default React.memo(function Logoanimation() {
   const [isStopped, setisStopped] = useState(false);
+  const container = useRef();
+
+  useEffect(() => {
+    const animation = lottie.loadAnimation({
+      container: container.current,
+      renderer: "svg",
+      loop: false,
+      autoplay: false,
+      animationData: animationData,
+      onComplete: () => console.log("hi"),
+    });
+    animation.play();
+    animation.addEventListener("complete", () =>
+      setTimeout(() => {
+        animation.stop();
+        animation.play();
+      }, 10000)
+    );
+  }, []);
 
   return (
     <div>
-      {/* <div className="wrapper">
-        <Lottie
-          options={defaultOptions}
-          height={250}
-          width={250}
-          isStopped={isStopped}
-          eventListeners={[
-            {
-              eventName: "complete",
-              callback: () => {
-                setTimeout(() => {
-                  setisStopped(true);
-                  setisStopped(false);
-                }, 20000);
-              },
-            },
-          ]}
-        />
-      </div> */}
+      <div className="wrapper logo" ref={container}></div>
     </div>
   );
 });
